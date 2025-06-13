eb29926980706684621b9687907e625766906804
PB <xachineon@gmail.com>
2025-06-13 18:33:21 +0530

    Topic created.

---
+++ src/main/java/com/kafkaesque/kafkaesque/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.kafkaesque.kafkaesque.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic basicTopic() {
+        return TopicBuilder
+                .name("kafkaesque")
+                .build();
+    }
+}
