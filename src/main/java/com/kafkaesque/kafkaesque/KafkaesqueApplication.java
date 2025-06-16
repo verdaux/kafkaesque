36fb2091a898759ff99b67cd5511729f7729e3ec
PB <xachineon@gmail.com>
2025-06-16 19:16:41 +0530

    Kafka terminal commands mentioned in comments.

--- src/main/java/com/kafkaesque/kafkaesque/KafkaesqueApplication.java
+++ src/main/java/com/kafkaesque/kafkaesque/KafkaesqueApplication.java
@@ -9,5 +9,19 @@ public class KafkaesqueApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaesqueApplication.class, args);
 	}
-
+	/*
+	 * Commands to start Kafka server
+	 * Start the kafka server
+	 * 1.
+	 * KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
+	 * 2.
+	 * bin/kafka-storage.sh format --standalone -t $KAFKA_CLUSTER_ID -c
+	 * config/server.properties
+	 * 3.
+	 * bin/kafka-server-start.sh config/server.properties
+	 * 
+	 * Display messages on the terminal
+	 * bin/kafka-console-consumer.sh --topic kafkaesque_json --from-beginning
+	 * --bootstrap-server localhost:9092
+	 */
 }
